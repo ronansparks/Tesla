d29527bd6a24e46259424434dac41375f01ee39b
ronansparks <ronansparks@gmail.com>
2018-07-01 22:22:32 +0800

    add SceneKit and AR

---
+++ Tesla/Controllers/Main/ARViewController.swift
@@ -0,0 +1,195 @@
+//
+//  ARViewController.swift
+//  Tesla
+//
+//  Created by Ronan on 7/1/18.
+//  Copyright © 2018 Ronan. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import RxSwift
+import RxCocoa
+
+enum MessageState {
+    case success
+    case fail
+}
+
+@available(iOS 11.0, *)
+class ARViewController: UIViewController, ARSCNViewDelegate {
+    
+    var sceneView: ARSCNView!
+    var messageLabel: UILabel!
+    var teslaNode: SCNNode? = nil
+    
+    var isTeslaPlaced = false
+    var debugPlanes: [SCNNode] = []
+    let bag = DisposeBag()
+    
+    var viewCenter: CGPoint {
+        let viewBounds = view.bounds
+        return CGPoint(x: viewBounds.width / 2.0, y: viewBounds.height / 2.0)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView = ARSCNView(frame: view.bounds)
+        sceneView.delegate = self
+        self.view.addSubview(sceneView)
+        runSession()
+        
+        setupSubviews()
+    }
+    
+    func setupSubviews() {
+        // 退出按钮
+        let exitButton = UIButton()
+        exitButton.setImage(UIImage(named: "close"), for: .normal)
+        self.view.addSubview(exitButton)
+        exitButton.snp.makeConstraints { make in
+            make.left.equalTo(UNIVERSAL_MARGIN)
+            make.top.equalTo(64)
+        }
+        exitButton.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.sceneView.session.pause()
+                self?.teslaNode?.removeFromParentNode()
+                self?.sceneView.removeFromSuperview()
+                self?.dismiss(animated: true, completion: nil)
+            })
+            .disposed(by: bag)
+        
+        // 提示标签
+        messageLabel = UILabel()
+        self.view.addSubview(messageLabel)
+        messageLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        setMessageLabel(text: nil, state: nil)
+    }
+    
+    // 设置提示标签
+    func setMessageLabel(text: String?, state: MessageState?) {
+        
+        guard let message = text, let messageState = state else {
+            messageLabel.alpha = 0
+            return
+        }
+        
+        messageLabel.text = message
+        messageLabel.alpha = 1
+        
+        switch messageState {
+        case .success:
+            messageLabel.backgroundColor = UIColor.themeGreen().withAlphaComponent(0.6)
+        case .fail:
+            messageLabel.backgroundColor = UIColor.themeRed().withAlphaComponent(0.6)
+        }
+        
+    }
+    
+    // 开始摄像头捕捉
+    func runSession() {
+        print(#function)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        configuration.isLightEstimationEnabled = true
+        
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        #if DEBUG
+            sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+        #endif
+    }
+    
+    // 点击放置 Tesla
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let hit = sceneView.hitTest(viewCenter, types: [.existingPlaneUsingExtent]).first {
+            sceneView.session.add(anchor: ARAnchor.init(transform: hit.worldTransform))
+        }
+    }
+    
+    // 创建 Tesla Node
+    func makeTesla() -> SCNNode {
+        let tesla = SCNNode()
+        
+        let scene = SCNScene(named: "art.scnassets/Model3.dae")
+        let rootNode = scene?.rootNode
+        rootNode?.scale = SCNVector3Make(0.01, 0.01, 0.01)
+        tesla.addChildNode(rootNode!)
+        
+        return tesla
+    }
+    
+    // session 中断后，移除
+    func removeAllNodes() {
+        self.teslaNode?.removeFromParentNode()
+        self.isTeslaPlaced = false
+    }
+    
+    deinit {
+        print(#function)
+    }
+}
+
+@available(iOS 11.0, *)
+extension ARViewController {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor, !self.isTeslaPlaced {
+                #if DEBUG
+                let debugPlaneNode = createPlaneNode(center: planeAnchor.center, extent: planeAnchor.extent)
+                node.addChildNode(debugPlaneNode)
+                self.debugPlanes.append(debugPlaneNode)
+                #endif
+                self.setMessageLabel(text: "点击屏幕，停放您的特斯拉", state: .success)
+            }
+            else if !self.isTeslaPlaced {
+                self.teslaNode = self.makeTesla()
+                if let tesla = self.teslaNode {
+                    node.addChildNode(tesla)
+                    self.isTeslaPlaced = true
+                    
+                    self.sceneView.debugOptions = []
+                    
+                    DispatchQueue.main.async {
+                        self.setMessageLabel(text: nil, state: nil)
+                    }
+                }
+            }
+        }
+    }
+    
+    // 更新平面
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            if let planeAnchor = anchor as? ARPlaneAnchor, node.childNodes.count > 0, !self.isTeslaPlaced {
+                updatePlaneNode(node.childNodes[0], center: planeAnchor.center, extent: planeAnchor.extent)
+            }
+        }
+    }
+    
+    // MARK: Session Handle
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        DispatchQueue.main.async {
+            self.setMessageLabel(text: "Session 加载失败", state: .fail)
+        }
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        DispatchQueue.main.async {
+            self.setMessageLabel(text: "Session 中断了", state: .fail)
+        }
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        DispatchQueue.main.async {
+            self.removeAllNodes()
+            self.setMessageLabel(text: "Session 已恢复", state: .success)
+        }
+        
+        runSession()
+    }
+}
--- Tesla/Controllers/Main/MainViewController.swift
+++ Tesla/Controllers/Main/MainViewController.swift
@@ -8,16 +8,22 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
+import ARKit
 
 class MainViewController: UIViewController {
 
-    let imageHeight: CGFloat = 225
+    let topHeight: CGFloat = 225
     let CellHeight: CGFloat = 56
     let imageWidth: CGFloat = 56
     
     var myCar: Car!
-    var carImageView: UIImageView!
+    
+    var topContainerView: UIView!
     var batteryView: BatteryView!
+    
+    let bag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,26 +36,49 @@ class MainViewController: UIViewController {
     func setupSubviews() {
         myCar = Car(model: .s, totalDistance: 570)
         myCar.availableDistance = 320
-        // 全景图片
-        carImageView = UIImageView()
-        view.addSubview(carImageView)
-        carImageView.snp.makeConstraints { make in
+        
+        topContainerView = UIView()
+        self.view.addSubview(topContainerView)
+        topContainerView.snp.makeConstraints { make in
             make.top.left.right.equalToSuperview()
-            make.height.equalTo(imageHeight)
+            make.height.equalTo(topHeight)
         }
-        carImageView.image = UIImage(named: "placeholder")
+        topContainerView.isUserInteractionEnabled = true
+        
+        setupScene()
+        
+        let button = UIButton()
+        topContainerView.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.right.equalToSuperview().offset(-UNIVERSAL_MARGIN)
+            make.bottom.equalToSuperview().offset(-UNIVERSAL_MARGIN)
+        }
+
+        if #available(iOS 11, *) {
+            let arVC = ARViewController()
+            button.setImage(UIImage(named: "home_ar"), for: .normal)
+            button.rx.tap
+                .subscribe(onNext: { [weak self] _ in
+                    self?.present(arVC, animated: true, completion: nil)
+                })
+                .disposed(by: bag)
+        }
+        else {
+            button.setImage(UIImage(named: "home_360"), for: .normal)
+            button.isEnabled = false
+        }
         
         // 电量
         batteryView = BatteryView()
         view.addSubview(batteryView)
         batteryView.snp.makeConstraints { make in
-            make.top.equalTo(carImageView.snp.bottom)
+            make.top.equalTo(topContainerView.snp.bottom)
             make.left.right.equalToSuperview()
             make.height.equalTo(CellHeight)
         }
         
         // 空调按钮
-        let fanContent = ButtonContent(normalImage: "home_fan_off", normalText: "26 ℃", selectedImage: "home_fan_on", selectedText: "20℃ xxxxx")
+        let fanContent = ButtonContent(normalImage: "home_fan_off", normalText: "26 ℃", selectedImage: "home_fan_on", selectedText: "22℃")
         let fanFrame = CGRect(x: 0, y: 0, width: SCREEN_WIDTH / 2, height: 129)
         let fanButton = CenterButton(frame: fanFrame, content: fanContent)
         view.addSubview(fanButton)
@@ -59,7 +88,7 @@ class MainViewController: UIViewController {
         }
         
         // 锁车/解锁
-        let lockContent = ButtonContent(normalImage: "home_lock", normalText: "Locked", selectedImage: "home_unlock", selectedText: "unlocked")
+        let lockContent = ButtonContent(normalImage: "home_lock", normalText: "Locked", selectedImage: "home_unlock", selectedText: "Unlocked")
         let lockFrame = CGRect(x: 0, y: 0, width: SCREEN_WIDTH / 2, height: 129)
         let lockButton = CenterButton(frame: lockFrame, content: lockContent)
         view.addSubview(lockButton)
@@ -86,6 +115,25 @@ class MainViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         batteryView.setBattery(car: myCar)
+    }
+    
+    func setupScene() {
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.camera?.zFar = 600
+        cameraNode.position = SCNVector3Make(0, 100, 200)
+        
+        let scene = SCNScene(named: "art.scnassets/Model3.dae")
+        scene?.rootNode.scale = SCNVector3Make(2, 2, 2)
+        
+        let sceneView = SCNView()
+        self.topContainerView.addSubview(sceneView)
+        sceneView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        sceneView.scene = scene
+        sceneView.backgroundColor = UIColor.white
+        sceneView.allowsCameraControl = true
     }
     
     deinit {
